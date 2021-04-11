897c352b46d9e5eb048d658be016cc640d4f3ae6
Stackofsugar <digno.christopher@gmail.com>
2021-04-11 13:15:33 +0700

    Updated: pch.h, main.cpp, .gitignore

--- include/pch.h
+++ include/pch.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <array>
 #include <string>
 #include <chrono>
+#include <ctime>
+#include <fstream>
 #include "../include/algorithms.h"
+
+#ifdef _MSC_VER && !__INTEL_COMPILER
+#pragma warning(disable:4996)
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,15 @@
 #include "../include/pch.h"
 using namespace std;
 
-/* Important Notes:
-1. Length of arrays will be always 10
-2. Testcases are hardcoded (except for random case)
-*/
-
 class Timer {
 private:
     chrono::time_point<chrono::high_resolution_clock> m_startTime;
     chrono::time_point<chrono::high_resolution_clock> m_endTime;
-    long long milliseconds;
+    long long ms;
+    long long us;
 public:
     Timer(bool immidiatelyStart) {
-        milliseconds = 0LL;
+        ms = 0LL;
         if (immidiatelyStart == true) {
             start();
         }
@@ -31,11 +27,16 @@ public:
         auto alpha = chrono::time_point_cast<chrono::microseconds>(m_startTime).time_since_epoch().count();
         auto omega = chrono::time_point_cast<chrono::microseconds>(m_endTime).time_since_epoch().count();
         auto duration = omega - alpha;
-        milliseconds = duration * 0.001;
+        ms = duration / 1000LL;
+        us = duration;
     }
 
-    void testPrint() {
-        cout << milliseconds;
+    long long getms() {
+        return ms;
+    }
+
+    long long getus() {
+        return us;
     }
 
     ~Timer() {
@@ -43,6 +44,47 @@ public:
     }
 };
 
-int main() {
-    
+void resPrintHandler(Timer &obj) {
+    cout << "Time: " << obj.getms() << " ms (" << obj.getus() << " us)\n";
 }
+
+void initTimeHandler(string &name) {
+    stringstream strs;
+    auto now = time(0);
+    tm *stm = localtime(&now);
+    //testmsg_DDMMYYYY_HH:MM:SS
+    strs << "res//testmsg_";
+    strs << setfill('0') << setw(2) << stm->tm_mday << flush;
+    strs << setfill('0') << setw(2) << 1 + stm->tm_mon << flush;
+    strs << 1900 + stm->tm_year << "_";
+    strs << setfill('0') << setw(2) << stm->tm_hour << flush;
+    strs << setfill('0') << setw(2) << stm->tm_min << flush;
+    strs << setfill('0') << setw(2) << stm->tm_sec << flush;
+    name = strs.str();
+}
+
+int main() {
+    string filename("");
+    initTimeHandler(filename);
+    ofstream file(filename);
+
+    // >>> Testrunning 1st Algorithm <<<
+    // 1. Building testcases
+    {
+        
+    }
+
+    file.close();
+}
+
+
+//Timer algo draft:
+//Timer testalg(false);
+//for (int i = 0; i < 5; i++) {
+//    cout << "\nRun #" << i + 1 << " with iter " << iter << '\n';
+//    testalg.start();
+//    testingAlgo2(iter);
+//    testalg.stop();
+//    resPrintHandler(testalg);
+//    iter *= 100;
+//}
